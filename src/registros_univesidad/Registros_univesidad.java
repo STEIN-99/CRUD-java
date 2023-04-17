c7e7ce630cd071088153aa5be9f4bd04faf5263d
Eric Alexis <eric.alexis0399@gmail.com>
2023-04-16 19:54:31 -0600

    Se realiza un formulario para mostrar campos de una tabla en mysql, se agrego la funcion de consultar y agregar campos a una sola tabla quedan pendiente las demas

---
+++ src/registros_univesidad/Registros_univesidad.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package registros_univesidad;
+
+/**
+ *
+ * @author Geo
+ */
+public class Registros_univesidad {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        formulario ver_form = new formulario();
+        ver_form.setVisible(true);
+        
+    }
+    
+}
